9aff8a199b733acc89b2dfe4ab6770261c54ef35
Marvin T <contact@marvinkturner.com>
2015-01-10 16:58:23 -0500

    Initial Commit

---
+++ hellowatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hellowatch
+//
+//  Created by Marvin T. on 1/10/15.
+//  Copyright (c) 2015 Marvin Turner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ hellowatchTests/hellowatchTests.swift
@@ -0,0 +1,36 @@
+//
+//  hellowatchTests.swift
+//  hellowatchTests
+//
+//  Created by Marvin T. on 1/10/15.
+//  Copyright (c) 2015 Marvin Turner. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class hellowatchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
