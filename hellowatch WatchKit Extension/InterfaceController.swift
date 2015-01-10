d0d45022db95b46e56a58ecd1f59e976e38b4ee3
Marvin T <contact@marvinkturner.com>
2015-01-10 17:32:16 -0500

    Updates layout

---
+++ hellowatch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  hellowatch WatchKit Extension
+//
+//  Created by Marvin T. on 1/10/15.
+//  Copyright (c) 2015 Marvin Turner. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
